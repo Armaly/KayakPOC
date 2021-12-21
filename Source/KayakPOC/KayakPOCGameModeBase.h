ef8a5bcbf1e8063fe878f5cc6051dea692bf1bf2
AA AA <armalyalbert@gmail.com>
2021-12-20 23:47:04 -0500

    Initial Commit

---
+++ Source/KayakPOC/KayakPOCGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KayakPOCGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KAYAKPOC_API AKayakPOCGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
